42442bff16d8f60e90b4d853f4ef3cf86f445f49
Fabián Guamán <fguamanf@gmail.com>
2022-10-13 19:20:07 -0300

    test producer

---
+++ src/test/groovy/cl/guaman/pocspringawskinesis/infrastructure/KinesisProducerSpec.groovy
@@ -0,0 +1,19 @@
+package cl.guaman.pocspringawskinesis.infrastructure
+
+import cl.guaman.pocspringawskinesis.domain.Message
+import spock.lang.Specification
+
+class KinesisProducerSpec extends Specification {
+
+    private KinesisProducer kinesisProducer
+
+    def "send message one time"() {
+        given:
+        kinesisProducer= new KinesisProducer()
+        Message message=new Message("1","hello world")
+        when:
+        kinesisProducer.send(message)
+        then:
+        0 * kinesisProducer.send(message)
+    }
+}
