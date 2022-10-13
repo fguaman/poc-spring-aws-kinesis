f564f3f9760255373991ae03fa53fa9894bbfa46
Fabián Guamán <fguamanf@gmail.com>
2022-10-13 16:54:18 -0300

    first test

--- src/test/java/cl/guaman/pocspringawskinesis/PocSpringAwsKinesisApplicationTests.java
+++ src/test/java/cl/guaman/pocspringawskinesis/PocSpringAwsKinesisApplicationTests.java
@@ -2,13 +2,11 @@ package cl.guaman.pocspringawskinesis;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PocSpringAwsKinesisApplicationTests {
 
     @Test
-    void contextLoads() {
+    void helloWorld() {
         String text = "Hello world!!!";
         Assertions.assertEquals("Hello world!!!", text);
     }
